c8d6a4cb8249f441d7f833fccca17cef29891989
syedmahmad <devm.ahmad@gmail.com>
2022-02-22 17:52:36 +0500

    used proper size images

--- src/singleton/headContent.js
+++ src/singleton/headContent.js
@@ -23,30 +23,28 @@ function HeadContent({
 			{/* MS Tile - for Microsoft apps */}
 			<meta name="msapplication-TileImage" content="http://www.example.com/image01.jpg"/> 
 
-			<meta property="og:site_name" content="Toppstation | Enabling Entrepreneurs worldwide"/>
+			{/* <!-- Primary Meta Tags --> */}
+			<title>{title}</title>
+			<meta name="title" content={title || "Toppstation | Enabling Entrepreneurs worldwide"} />
+			<meta name="description" content={description || "Discover benefits of building a business from a pro-business environment, including how tax incentives, rule of law, connectivity to other markets, strategic location and skilled workforce benefit you regardless where you are."} />
+
+			{/* <!-- Open Graph / Facebook --> */}
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content={url + route} />
+			<meta property="og:site_name" content="Toppstation"/>
 			<meta property="og:title" content={title || "Toppstation | Enabling Entrepreneurs worldwide"} />
 			<meta property="og:description" content={description || "Discover benefits of building a business from a pro-business environment, including how tax incentives, rule of law, connectivity to other markets, strategic location and skilled workforce benefit you regardless where you are."} />
-			<meta property="og:image" itemProp="image" content={"/images/300.png"}/>
-			<meta property="og:type" content="website" />
+			<meta property="og:image" itemProp="image" content={"/images/blog-5.jpg"}/>
+			
 			<link rel="canonical" href={url + route} />
 			{/* <meta property="fb:app_id" content="1757891891170883" /> */}
-			{/* No need to change anything here  */}
-			<meta property="og:type" content="website" />
-			<meta property="og:image:type" content="image/jpeg"/>
-
-			{/* <!-- Size of image. Any size up to 300. Anything above 300px will not work in WhatsApp --> */}
-			<meta property="og:image:width" content="300"/>
-			<meta property="og:image:height" content="300"/>
-
-			{/* <!-- Website to visit when clicked in fb or WhatsApp--> */}
-			<meta property="og:url" content={url + route} />
 
 			{/* Twitter special case  */}
-			<meta name="twitter:card" content="summary_large_image"/>
-			<meta name="twitter:site" content="Toppstation | Enabling Entrepreneurs worldwide"/>
-			<meta name="twitter:title" content={title || "Toppstation | Enabling Entrepreneurs worldwide"}/>
-			<meta name="twitter:description" content={description || "Discover benefits of building a business from a pro-business environment, including how tax incentives, rule of law, connectivity to other markets, strategic location and skilled workforce benefit you regardless where you are."} />
-			<meta name="twitter:image" content="/images/300.png"/>
+			<meta property="twitter:card" content="summary_large_image"/>
+			<meta property="twitter:url" content={url + route}/>
+			<meta property="twitter:title" content={title || "Toppstation | Enabling Entrepreneurs worldwide"}/>
+			<meta property="twitter:description" content={description || "Discover benefits of building a business from a pro-business environment, including how tax incentives, rule of law, connectivity to other markets, strategic location and skilled workforce benefit you regardless where you are."} />
+			<meta property="twitter:image" content="/images/blog-5.jpg"/>
 		</Head>
 	);
 }
